8f02f24ff9c8640a15387e4866632baf4e7206cb
Sergey Chikuyonok <serge.che@gmail.com>
2016-10-19 01:10:54 +0300

    i18n

--- apply-anim.js
+++ apply-anim.js
@@ -1,12 +1,16 @@
 (function() {
+    var supportedLangs = /^ru|en$/;
     var anim = location.hash.replace('#', '');
     if (!anim) {
         return;
     }
 
     var parts = anim.split(':');
+    var lang = supportedLangs.test(parts[0] || '') ? parts.shift() : 'en';
     var elem = parts[0];
     var animClass = parts[1];
+
+    document.documentElement.setAttribute('lang', lang);
 
     if (elem && animClass) {
         var items = document.querySelectorAll(elem);
